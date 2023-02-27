f7d35b10f79d0b178040fa22c50a7368744a888b
pairofAces <karanchauhan16@gmail.com>
2023-02-26 22:23:19 -0500

    created Departments and Employees Models

--- djangoAPI/djangoAPI/settings.py
+++ djangoAPI/djangoAPI/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corshearders',
+    'EmployeeApp.apps.EmployeeappConfig'
 ]
 
+#Not recommended for production - this is only for current practice
+    #Usually the domains that need to be whitelisted will be listed here
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
